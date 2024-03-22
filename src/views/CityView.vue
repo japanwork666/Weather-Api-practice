134674ef441e8d1fc95791fd14fe927ad48d9eb5
John <japanwork6@gmail.com>
2024-03-22 21:36:01 +0800

    completed app

---
+++ src/views/CityView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- 利用vue 內建 Suspense tag 加載中狀態 -->
+        <!-- 有两個插槽：#default 和 #fallback -->
+        <Suspense>
+            <AsyncCityView />
+            <template #fallback>
+                <CityViewSkeleton />
+            </template>
+        </Suspense>
+    </div>
+</template>
+
+<script setup>
+import AsyncCityView from '@/components/AsyncCityView.vue';
+import CityViewSkeleton from '@/components/CityViewSkeleton.vue';
+
+</script>
